038fcfb6bb69a1b344ca5bd54c9ff209ebd1fb36
Michael Monetized <michaelmonetized@gmail.com>
2025-03-09 01:58:51 -0500

    testing font weights

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-dvh">
       <Image src="/chicken.svg" alt="Zaxby's" width={95} height={95} />
-      <h1 className="text-4xl font-bold font-sans">{"ZAXBYS"}</h1>
+      <h1 className="text-4xl font-black font-sans">{"ZAXBYS"}</h1>
       <h2 className={`text-8xl font-casual`}>{"Hello World"}</h2>
     </div>
   );
