3beca9576565e0b5cca45deef48e1aa0700829d9
Michael Monetized <michaelmonetized@gmail.com>
2025-03-09 03:06:44 -0400

    fonts working, local not working, remote working

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-dvh">
       <Image src="/chicken.svg" alt="Zaxby's" width={95} height={95} />
-      <h1 className="text-4xl font-black font-sans">{"ZAXBYS"}</h1>
-      <h2 className={`text-8xl font-casual`}>{"Hello World"}</h2>
+      <Image src="/logo.svg" alt="Zaxby's" width={210} height={51} />
+      <h1 className={`text-4xl font-black capitalize`}>{"Southern Fried"}</h1>
+      <h2 className={`text-10xl font-casual`}>{"Shrimp"}</h2>
     </div>
   );
 }
